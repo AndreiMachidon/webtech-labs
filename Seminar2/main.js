548014ce036fe7b2c081a7f00bd73842dfac7840
andrei <andreimachidon@gmail.com>
2023-10-10 15:05:03 +0300

    seminar2

---
+++ Seminar2/main.js
@@ -0,0 +1,16 @@
+let y  = "y"
+const x = "x";
+var z = "z";
+
+const myCar = {
+    color: 'red',
+    year: 2020,
+    brand: 'Toyota',
+    doSomething: (a) => {
+        console.log("do " + a)
+    }
+}
+
+console.log(myCar.doSomething("javascript"));
+
+
